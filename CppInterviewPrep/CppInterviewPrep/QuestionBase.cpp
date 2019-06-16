52cc301033cdf1297be4bd88eac9b0a36d4b16b1
POWER-PC\shay <shay.blum.1@gmail.com>
2019-06-16 22:50:29 +0300

    added base class for question code

---
+++ CppInterviewPrep/CppInterviewPrep/QuestionBase.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "QuestionBase.h"
+
+
+QuestionBase::QuestionBase() {
+}
+
+
+QuestionBase::~QuestionBase() {
+}
+
+void QuestionBase::RunQuestionCode() {
+}
---
+++ CppInterviewPrep/CppInterviewPrep/QuestionBase.h
@@ -0,0 +1,11 @@
+
+
+#pragma once
+class QuestionBase
+{
+public:
+	QuestionBase();
+	~QuestionBase();
+	void RunQuestionCode();
+};
+
